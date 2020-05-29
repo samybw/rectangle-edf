9947927e685b9216d5a89e7a45c2c41896e2955c
samy <samybn@yahoo.com>
2020-05-29 11:13:05 +0200

    terminé !  création des fonction permettant de parcourir le tableau et de trouve la position

--- rectangle.js
+++ rectangle.js
@@ -2,33 +2,70 @@ var fs = require('fs');
 
 
 const data = fs.readFileSync('c1.txt', 'UTF-8');
-const findThis = data.split(/\r?\n/).map((element) => { 
+const c1 = data.split(/\r?\n/).map((element) => { 
     var newElement = element.split('').map(Number) ;
     return newElement;
 })
 
-console.log(findThis);
+//console.log(c1);
 
 const data2 = fs.readFileSync('c2.txt', 'UTF-8');
-const inThat = data2.split(/\r?\n/).map((element) => { 
+const c2 = data2.split(/\r?\n/).map((element) => { 
     var newElement = element.split('').map(Number) ;
     return newElement;
 })
 
-console.log(inThat);
+//console.log(c2);
+function match(c1 , c2, ligne, colonne) {
+    ligne2 = 0 
+while (ligne2 < c1.length) {
+   colonne2 = 0
 
-var parcourirl = 0 ; 
-var parcourirc = 0 
-while (parcourirl < inThat.length) {
-   
-    while (parcourirc < inThat[parcourirl]) {
-    console.log(parcourc);
-     parcourirc++
+    while (colonne2 < c1[ligne2].length) {
+        if (c1[ligne2][colonne2] != c2[ligne + ligne2][colonne + colonne2]) {
+            return false
+        }
+        
+        
+        colonne2++
     }
-  parcourirl++
-    
-    
+    ligne2++
 }
+return true 
+}
+
+function rectangle(c1 , c2) {
+    var ligne = 0 ; 
+while (ligne < c2.length) {
+    var colonne = 0 ;
+    while (colonne < c2[ligne].length) {
+    if (c2[ligne][colonne] == c1[0][0] ) {
+    if (match(c1 , c2, ligne, colonne)) {
+        console.log([colonne]+ ',' +[ligne]);
+        return true
+     }
+   
+    }
+    colonne++
+     }
+ ligne++
+    }
+    console.log("Pas de Match");
+return false
+  
+}
+
+rectangle(c1 ,c2)
+
+
+     
+    
+
+
+    
+    
+
+
 
 
   
