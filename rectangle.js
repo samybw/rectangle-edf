2b79d2591bc9b38945cb92f1690332cdad5a054c
samy <samybn@yahoo.com>
2020-05-29 09:41:44 +0200

    creation de la boucle

--- rectangle.js
+++ rectangle.js
@@ -2,20 +2,33 @@ var fs = require('fs');
 
 
 const data = fs.readFileSync('c1.txt', 'UTF-8');
-const lines = data.split(/\r?\n/).map((element) => { 
+const findThis = data.split(/\r?\n/).map((element) => { 
     var newElement = element.split('').map(Number) ;
     return newElement;
 })
 
-console.log(lines);
+console.log(findThis);
 
 const data2 = fs.readFileSync('c2.txt', 'UTF-8');
-const lines2 = data2.split(/\r?\n/).map((element) => { 
+const inThat = data2.split(/\r?\n/).map((element) => { 
     var newElement = element.split('').map(Number) ;
     return newElement;
 })
 
-console.log(lines2);
+console.log(inThat);
+
+var parcourirl = 0 ; 
+var parcourirc = 0 
+while (parcourirl < inThat.length) {
+   
+    while (parcourirc < inThat[parcourirl]) {
+    console.log(parcourc);
+     parcourirc++
+    }
+  parcourirl++
+    
+    
+}
 
 
   
